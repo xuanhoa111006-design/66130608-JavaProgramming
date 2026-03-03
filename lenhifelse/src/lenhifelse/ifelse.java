7f571be34cc832888d4e70c0d8d78c638aa0a3bf
lequangdung1507 <lequangdung1507@gmail.com>
2026-03-03 13:58:18 +0700

    full code

--- lenhifelse/src/lenhifelse/ifelse.java
+++ lenhifelse/src/lenhifelse/ifelse.java
@@ -3,8 +3,14 @@ package lenhifelse;
 public class ifelse {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int x = 10;
 
+		if (x > 5) {
+		    System.out.println("x lớn hơn 5");
+		} else {
+			System.out.println("x nhỏ hơn 5");
+		}
 	}
 
 }
+
