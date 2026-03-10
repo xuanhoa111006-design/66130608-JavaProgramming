9569773d13bbcd646b737316e76159322a46ce4a
lequangdung1507 <lequangdung1507@gmail.com>
2026-03-10 20:35:44 +0700

    Update code OOP  VD2

---
+++ ViDu2_OOP_Getter/src/ChuNhat.java
@@ -0,0 +1,14 @@
+
+public class ChuNhat {
+	//Khai bao cac thuoc tinh / du lieu cua lop / truong thong tin
+	private double dai;
+	private double rong;
+	
+	// tao ham constructor
+	public ChuNhat() {
+		dai = 10;
+		rong = 5;
+	}
+	public ChuNhat(double dai, double rong)
+
+}
