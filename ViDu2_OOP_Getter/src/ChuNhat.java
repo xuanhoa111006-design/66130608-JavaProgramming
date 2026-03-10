c23b57f255066654ae441612d84b5fd4f0d3e464
lequangdung1507 <lequangdung1507@gmail.com>
2026-03-10 20:51:21 +0700

    update code ChuNhat

--- ViDu2_OOP_Getter/src/ChuNhat.java
+++ ViDu2_OOP_Getter/src/ChuNhat.java
@@ -9,6 +9,29 @@ public class ChuNhat {
 		dai = 10;
 		rong = 5;
 	}
-	public ChuNhat(double dai, double rong)
-
+	public ChuNhat(double dai, double rong) {
+        this.dai = dai;
+        this.rong = rong;
+    }
+    //cach 2
+//    public ChuNhat(double daiM, double rongM) {
+//        dai = daiM;
+//        rong = rongM;
+//    }
+    // cac getter
+    doube getDai() {
+        return dai;
+    }
+    double getRong() {
+        return rong;
+    }
+    // cac setter
+    void setDai(double daiM) {
+        dai = daiM;
+    }
+    void setRong(double rongM) {
+        rong = rongM;
+    }
+    //cac phuong thuc khac
+    
 }
---
+++ ViDu2_OOP_Getter/src/run.java
@@ -0,0 +1,9 @@
+
+public class run {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
